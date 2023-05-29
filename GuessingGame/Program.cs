86d79af7289a1be2677806db5ca5e9f811eabedb
David Profitt <dlprofitt@gmail.com>
2023-05-28 23:08:47 -0400

    Phase 7 completed

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -6,39 +7,65 @@ class Program
     {
         Random num = new Random();
         int secret = num.Next(1, 100);
-        int guessMax = 4;
-        Console.WriteLine("Bet you can't guess my secret number!");
-        for (int i = 0; i < 4; i++)
-        {
-            int guessesRemaining = guessMax--;
-            Console.WriteLine();
-            Console.WriteLine($"You have {guessesRemaining} guesses.");
-            Console.WriteLine("What number am I thinking of?");
-            string guessString = Console.ReadLine();
+        int guessMax = 8;
+        int iterator = 8;
+        Console.WriteLine("Let's play a guessing game.");
+        Console.WriteLine("Pick a difficulty level to get started:");
+        Console.WriteLine("1) Easy");
+        Console.WriteLine("2) Medium");
+        Console.WriteLine("3) Hard");
+        string levelSelection = Console.ReadLine();
+        int difficultyLevel = int.Parse(levelSelection);
 
-            bool parseSuccess = Int32.TryParse(guessString, out int guess);
-            if (parseSuccess)
+        if (difficultyLevel >= 1 && difficultyLevel <= 4) 
+        {
+            if (difficultyLevel != 1)
             {
-                if (guess == secret)
+                if (difficultyLevel == 2)
                 {
-                    Console.WriteLine("Whoa, are you psychic?!");
-                    Console.WriteLine("I'm impressed. Let's play again soon.");
-                    return;
+                    guessMax = 6;
+                    iterator = 6;
                 }
                 else
                 {
-                    if (i < 3)
-                    {
-                        Console.WriteLine(guess < secret ? "Uh... too low." : "Nope... too high");
-                    }
+                    guessMax = 4;
+                    iterator = 4;
                 }
             }
-            else
+
+            for (int i = 0; i < iterator; i++)
             {
-                Console.WriteLine("Invalid input. Please enter a number.");
-                i--;
+                int guessesRemaining = guessMax--;
+
+                Console.WriteLine();
+                Console.WriteLine($"You have {guessesRemaining} guess(es).");
+                Console.WriteLine("What number am I thinking of?");
+                string guessString = Console.ReadLine();
+
+                bool parseSuccess = Int32.TryParse(guessString, out int guess);
+                if (parseSuccess)
+                {
+                    if (guess == secret)
+                    {
+                        Console.WriteLine("Whoa, are you psychic?!");
+                        Console.WriteLine("I'm impressed. Let's play again soon.");
+                        return;
+                    }
+                    else
+                    {
+                        if (i < guessMax)
+                        {
+                            Console.WriteLine(guess < secret ? "Uh... too low." : "Nope... too high");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    i--;
+                }
             }
-        }
+        } 
 
         Console.WriteLine("You lose! Better luck next time! Buh-bye");
     }
